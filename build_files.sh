883e4327ac97eaf4133332caa5a80fcacb17d548
Atharva Nikam <atharvanikam8055@gmail.com>
2025-10-28 21:37:53 +0530

    deployment

---
+++ build_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+echo "Building project..."
+
+# Install Python dependencies
+pip install -r Requirement.txt
+
+# Run Django collectstatic
+python manage.py collectstatic --noinput
